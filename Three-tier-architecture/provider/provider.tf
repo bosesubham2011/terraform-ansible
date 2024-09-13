ba64a4cdd8c61bd579cd43f2a23caf8b2b23cb64
bosesubham2011 <bosesubham2011@gmail.com>
2024-09-12 20:35:39 -0700

    Provider commit 1

--- Three-tier-architecture/provider/provider.tf
+++ Three-tier-architecture/provider/provider.tf
@@ -1,15 +1,7 @@
-variable "access_key"{
 
-}
-variable "secret_key"{
 
-}
-variable "region"{
-
-}
-
-provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region     = var.region
+terraform {
+  required_providers {
+    aws = "~> 1.0"
+  }
 }
